728d38126209f419765e7225d18ef88bc1bdaed1
susshikan <muhamadh123g@gmail.com>
2025-11-12 16:38:03 +0700

    tutotrial 8 selesai

--- football_news/lib/screens/news_entry_list.dart
+++ football_news/lib/screens/news_entry_list.dart
@@ -19,7 +19,7 @@ class _NewsEntryListPageState extends State<NewsEntryListPage> {
     // To connect Android emulator with Django on localhost, use URL http://10.0.2.2/
     // If you using chrome,  use URL http://localhost:8000
     
-    final response = await request.get('http://[YOUR_APP_URL]/json/');
+    final response = await request.get('http://localhost:8000/json/');
     
     // Decode response to json format
     var data = response;
